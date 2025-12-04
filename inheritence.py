38b4c0c42d97d81c5c86b128411f6685423ce081
Pavan Kumar <kpavan36668@gmail.com>
2025-12-04 12:57:16 +0530

    inheritance updated

--- inheritence.py
+++ inheritence.py
@@ -66,3 +66,84 @@
 # ob_c=Director()
 # ob_c.budget()
 # ob_c.movie_info2("sahoo",2013,"Prabhas")
+
+# super with variables
+# class A:
+#     age=21
+    
+# class B(A):
+#     age=22
+#     def show(self):
+#         print(self.age)
+#         print(super().age)
+
+# obj=B()
+# obj.show()
+
+# # super with method
+# class A:
+#     def addition(self):
+#         print("Sum: ",20+30)
+# class B(A):
+#     def subraction(self):
+#         super().addition()
+#         print("Subraction: ",30-10)
+# obj=B()
+# obj.subraction()
+
+
+# super() with constructor
+# class A:
+#     def __init__(self):
+#         print("I am parent class constructor")
+# class B(A):
+#     def __init__(self):
+#         print("I am child class constructor")
+#         super().__init__()
+# obj=B()
+
+
+# single inheritance
+# class A:
+#     def display(self):
+#         print("I am parent method")
+# class B(A):
+#     def display(self):
+#         print("I am a child class ")
+#         super().display()
+# obj=B()
+# obj.display()
+
+# multilevel inheritance
+# class A:
+#     def display(self):
+#         print("I am Grand parent method")
+# class B(A):
+#     def display(self):
+#         print("I am a parent  method ")
+#         super().display()
+# class C(B):
+#     def display(self):
+#         print("I am a child class method")
+#         super().display()
+# obj=C()
+# obj.display()
+
+# multiple inheritance
+# class A:
+#     def display1(self):
+#         print("I am Grand parent1 method")
+# class B:
+#     def display2(self):
+#         print("I am Grand parent2 method")
+# class C(A,B):
+#     def display(self):
+#         print("I am a child class method")
+#         super().display1()
+#         super().display2()
+# obj=C()
+# obj.display()
+
+# Multiple inheritance
+
+
