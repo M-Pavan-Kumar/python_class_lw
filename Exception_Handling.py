1c642af9293b16927f94f583f31a486e03961c5e
Pavan Kumar <kpavan36668@gmail.com>
2025-12-22 12:23:21 +0530

    updated exception handling

--- Exception_Handling.py
+++ Exception_Handling.py
@@ -1,20 +1,51 @@
-try:
-    # print(2/0)
-    n1=int(input("Enter a value : "))
-    n2=int(input("Enter  a value : "))
-    # print(n1+n2)
-    print(m)
+# try:
+#     # print(2/0)
+#     n1=int(input("Enter a value : "))
+#     n2=int(input("Enter  a value : "))
+#     # print(n1+n2)
+#     # print(m)
 
     
-except ZeroDivisionError:
-    print("Zero division Error occured")
+# except ZeroDivisionError:
+#     print("Zero division Error occured")
+# except NameError:
+#     print("Name error occured")
+# except TypeError:
+#     print("Type error occured")
+# except ValueError:
+#     print("value Error occured")
+# except:
+#     print("This is final except")
+# finally:
+#     print("Final block")
+
+
+# lst=[5,6,0,10,5]
+# for req in lst:
+#     try:
+#         n=10000
+#         res=n/req
+#         print("Processed successfully : ",res)
+#     except ZeroDivisionError:
+#         print("Zero Division error occured")
+#     else:
+#         print("This is else")
+
+
+try:
+    a=int(input())
+    b=int(input())
+    p=a*b
+    print(p)
 except NameError:
-    print("Name error occured")
+    print("Name Error occured ")
 except TypeError:
     print("Type error occured")
 except ValueError:
     print("value Error occured")
 except:
     print("This is final except")
-finally:
-    print("Final block")
+else:
+    print("This is else block ")
+
+    
