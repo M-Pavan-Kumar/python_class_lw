83b04ba89d128a1fb66c36fd9698be939ae85a4b
Pavan Kumar <kpavan36668@gmail.com>
2025-12-23 08:15:29 +0530

    Iterators learned

---
+++ Iterators.py
@@ -0,0 +1,23 @@
+# it=iter([1,2,3,4,5])
+# print(next(it))
+# print(next(it))
+# print(next(it))
+# print(next(it))
+# print(next(it))
+
+# it=iter([5,4,3,2,1])
+# for n in it:
+#     print(n)
+
+# Iterator with Strings
+# s="pavan"
+# it=iter(s)
+# for ch in it:
+#     print(ch)
+
+# Normal list
+# nums=[i for i in range(10000)]
+# print(nums)
+
+# Iterator
+nums=range(10000)
